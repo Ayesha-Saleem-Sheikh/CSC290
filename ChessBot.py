c6b4e2405c27901bfcca5c9ab1681ec65c45b4b1
Binah Friedman <bkfriedman@smith.edu>
2025-09-25 16:40:29 -0400

    lots of progress (with kira and ayesha)

--- ChessBot.py
+++ ChessBot.py
@@ -1,32 +1,77 @@
 import chess
+import random
+import player
 
 
+def pick_move(board: chess.Board):
+    moves = []
+    max_move_score = -99
+    for m in board.legal_moves: # for each legal move
+        #calculate the move's score
+        current_move_score = 0 #reset the score
+        if board.is_capture(m): #if the move captures a piece
+            current_move_score += 1 #increase its score by one
+            #add variable increases based on captured piece?
+        if board.is_en_passant(m): #if the move is en passant
+            current_move_score += 1 #increase its score by one (will total two bc en passant is necesarily a capture)
+            #because funny. everybody loves en passant :3
+        if board.gives_check(m): #if the move puts the other side in check
+            current_move_score += 3 #increase its score by three
+        #add or dont based on move's score
+        if current_move_score > max_move_score: #if m is better than the current movepool
+            max_move_score = current_move_score #update max move score
+            moves = [] #empty the movepool
+        if current_move_score == max_move_score:
+            moves.append(m) #add m to movepool
+    return moves[random.randrange(0,len(moves))]
+
+
+
+
+#main action starts here
 board = chess.Board()
+x = input("Computer Player? (w=white/b=black):") #get player and store as variable
+if x == 'w':#choose a side
+    player_color = chess.WHITE
+else:
+    player_color = chess.BLACK
+computer_player = player(player_color, board)
+human_player = player(not player_color, board)
+print("Starting FEN position? (hit ENTER for standard starting position):")
+pos = input()
+try: #try to set the board state from the input
+    board.set_fen(pos)
+except ValueError: #if the 
+    if (not pos == ""): #(dont show error if they hit enter like the instructions said)
+        print("Invalid FEN! Starting from default board.") #show error and continue
 print(board)
-# player = input("Computer Player? (w=white/b=black):") #get player and store as variable
-# print("Starting FEN position? (hit ENTER for standard starting position):")
-
-# if (position.equals("")):
-#if user enters something, set the board state
+if computer_player.color: #if computer is playing as white, make the first move
+    move = pick_move(board)
+    board.push(move)
+    print(board)
+    print(board.fen())
 #gameplay loop:
-#while not in checkmate:
-#
-while (True):
-    position = input("White:")
-    move = chess.Move.from_uci(position)
+while (not board.is_game_over()): #while not in checkmate/stalemate/etc
+    if human_player.color:
+        move = input("White:")
+    else:
+        move = input("Black:")
+    human_player.make_move(board, move)
+    print(board)
+    print(board.fen())
+    move = pick_move(board)
     board.push(move)
     print(board)
     print(board.fen())
-    position = input("Black:")
-    move = chess.Move.from_uci(position)
-    board.push(move)
-    print(board)
-    print(board.fen())
-# while (True):
+
     
     
 
 
+
+        
+
     
+
 
 
---
+++ player.py
@@ -0,0 +1,53 @@
+import chess
+class player:
+    def __init__(self, color: bool, board: chess.Board):
+     self.board = board
+     self.color = color
+    
+    def make_move(self,board: chess.Board,move):
+        piece =board.piece_at(move.from_square)
+        if piece.color == self.color:
+           board.push(move)
+           return move
+        else:
+            raise Exception("It's not your turn!")  
+            
+
+game = chess.Board()
+print(game)
+x = input ("Enter your color (w=white/b=black): ")
+if x == 'w':
+    player_color = chess.WHITE
+else:
+    player_color = chess.BLACK
+
+
+
+p1 = player(player_color, game)
+p2 = player(not player_color,game)
+print ("Player 1 is playing as :" , p1.color)
+print("player 2 is playing as :", p2.color)
+while not game.is_game_over():
+    if game.turn == chess.WHITE:
+        p1.make_move(game)
+    else:
+        p2.make_move(game)
+    print(game)
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
